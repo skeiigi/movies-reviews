856f6e22690e87b7a1a03591769152ec55343f1b
neithiri <popokakapo76xxx@gmail.com>
2025-05-01 01:22:38 +0700

    добавил вывод даты добавления и последнего изменения

--- src/moviesApp/models.py
+++ src/moviesApp/models.py
@@ -22,6 +22,7 @@ class Reviews(models.Model):
     )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, null=False)
+    changed_at = models.DateTimeField(auto_now=True, null=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
